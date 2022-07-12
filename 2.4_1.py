2388cb3baf59ccbe925823b105f1e106cbff4c58
ayji <aiji.ulanova@gmail.com>
2022-07-12 19:12:42 +0600

    добавлены комментарии

--- 2.4_1.py
+++ 2.4_1.py
@@ -3,13 +3,16 @@ from selenium.webdriver.common.by import By
 import time
 
 try:
+    #открытие ссылки
     link = "http://suninjuly.github.io/cats.html"
     browser = webdriver.Chrome()
     browser.get(link)
+    #нажатие на нужную кнопку
     button = browser.find_element(By.ID, "button")
     button.click()
 
 finally:
+    #закрытие страницы через 6 секунд
     time.sleep(6)
     browser.quit()
 
